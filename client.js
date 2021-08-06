bbabe847a3a95fb3f0d416eec2f5acabc6f8cba1
Hannah Fligel <fligelhannah@gmail.com>
2021-08-06 13:59:25 -0500

    done through 17

---
+++ client.js
@@ -0,0 +1,25 @@
+console.log("Hello world");
+
+variables //
+let firstName = "Hannah";
+console.log(firstName); 
+firstName = "Stav";
+console.log(firstName);
+let lastName = 1;
+console.log(lastName);
+firstName = 2;
+console.log(firstName);
+console.log(firstName, lastName);
+console.log("Hi my name is", firstName, lastName);
+
+// Create variables for your name, the number of cats you have, floyd, and moon. Then display the sentence "Hi my name is Hannah and I have 2 cats named Moon and Floyd"
+
+// const firstName = "Hannah"
+// console.log(firstName);
+// let catNumber = 2;
+// let catOne = "Moon"
+// console.log(catOne);
+// let catTwo = "Floyd"
+// console.log(catOne);
+// console.log("Hi my name is", firstName, "and I have", catNumber, "cats named", catOne, "and", catTwo);
+
